9d9e7fc8755efb3d8141fe6ed17b63cea7b9e20e
axel <shemaromeoaxel@gmail.com>
2019-08-16 12:15:57 +0200

    geolib added

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,6 @@
-var ObjectID = require("mongodb").ObjectID;
 const log = require("../../config/logging");
-
+const geolib = require("geolib");
+//
 module.exports = function(app, db) {
   //all location byname
   app.get("/suggestions/:q/:lat/:lon", (req, res) => {
@@ -20,11 +20,26 @@ module.exports = function(app, db) {
           log.error("An error occured :" + err.toJSON());
           console.log(err);
         } else {
-          //sort in ascending
-          items.sort(GetSortOrder("name"));
           //calculate score
+          log.info("Calculating the score");
+          items.forEach(element => {
+            const dist =
+              geolib.getDistance(
+                { latitude: lat, longitude: lon },
+                { latitude: element.latitude, longitude: element.longitude }
+              ) / 1000; //distance in km
+            element.score = calculateScore(dist);
+          });
+          //sorting array ascending
+          log.info("Sorting the elements");
+          items.sort(function(a, b) {
+            return parseFloat(b.score) - parseFloat(a.score);
+          });
           log.info("Success response :" + JSON.stringify(items));
-          res.send(items);
+          response = {
+            suggestions: items
+          };
+          res.send(response);
         }
       });
   });
@@ -40,14 +55,15 @@ module.exports = function(app, db) {
           console.log(err);
         } else {
           log.info("Success Response :" + JSON.stringify(items));
+          items.sort(function(a, b) {
+            return parseFloat(b.score) - parseFloat(a.score);
+          });
           res.send(items);
         }
       });
   });
 
   app.post("/locations", (req, res) => {
-    console.log(req.body);
-
     const location = {
       name: req.body.name,
       latitude: req.body.latitude,
@@ -65,13 +81,25 @@ module.exports = function(app, db) {
   });
 };
 
-function GetSortOrder(prop) {
-  return function(a, b) {
-    if (a[prop] > b[prop]) {
-      return 1;
-    } else if (a[prop] < b[prop]) {
-      return -1;
-    }
-    return 0;
-  };
+//score calculator
+function calculateScore(dist) {
+  if (dist < 10) {
+    return 0.9;
+  } else if (dist < 25) {
+    return 0.8;
+  } else if (dist < 50) {
+    return 0.7;
+  } else if (dist < 75) {
+    return 0.6;
+  } else if (dist < 100) {
+    return 0.5;
+  } else if (dist < 125) {
+    return 0.4;
+  } else if (dist < 150) {
+    return 0.3;
+  } else if (dist < 200) {
+    return 0.2;
+  } else {
+    return 0.1;
+  }
 }
